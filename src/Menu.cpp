34e04e5399cbc5ea446fae0e8731b269ffe6295b
Milka <201422>
2016-06-21 02:01:39 -0400

    Library with  method to list the book

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -1,23 +1,54 @@
-/*
- * Menu.cpp
- *
- *  Created on: May 23, 2016
- *      Author: raydelto
- */
-
-#include "Menu.h"
-#include <iostream>
-
-using namespace std;
-
-void Menu::show()
-{
-	//TODO: Implement this method.
-	cout << "Implement the menu" << endl;
-}
-
-Menu::Menu()
-{
-
-}
-
+void Menu::listItems()
+  {
+ -
+ -	cout << "List Book" << endl;
+ +    Stack* stack = new Stack();
+ +    Element* i = _stack -> pop();
+ +        while(i != NULL)
+ +			{
+ +				 cout << i->getName() << "\t" << i->getAuthor() << "\t" << i-> getPublisher()<< "\t" << i->getDate() << "\t" << i->getCategory() << endl;
+ +				 i = _stack->pop();
+ +			}
+ +	//cout << "List Book" << endl;
+  }
+ +Stack* _stack = new Stack();
+  
+  void Menu::addItems()
+  {
+ +    std::string name, author, publisher, date, category;
+ +
+ +	cout <<"Write book's name: "<<endl;
+ +	cin>>name;
+ +
+ +	cout <<"Write Author's name:"<<endl;
+ +	cin>>author;
+  
+ -	cout << "Add Book" << endl;
+ +	cout<<"Write Publisher: "<<endl;
+ +	cin>>publisher;
+ +
+ +	cout <<"Write published date: "<< endl;
+ +	cin>>date;
+ +
+ +	cout <<"Write category: "<<endl;
+ +	cin>>category;
+ +
+ +	Element* book = new Element(name, author , publisher,date,category);
+ +
+ +	 _stack->push(book);
+ +	//cout << "Add Book" << endl;
+  }
+  
+  void Menu::requestLastBook()
+  {
+ -	cout << "Request a Book"<< endl;
+ +    Element* i = _stack -> pop();
+ +			while(i != NULL)
+ +			{
+ +			 cout << i->getName() << "\t" << i->getAuthor() << "\t" << i-> getPublisher()<< "\t" << i->getDate() << "\t" << i->getCategory() << endl;
+ +					 i = _stack->pop();
+ +				}
+ +	//cout << "Request a Book"<< endl;
+  }
+  
+  void Menu::searchByCategory()
--- src/Stack.h
+++ src/Stack.h
@@ -1,21 +1,14 @@
-/*
- * Stack.h
- *
- *  Created on: May 23, 2016
- *      Author: raydelto
- */
-
-#ifndef STACK_H_
-#define STACK_H_
-#include "Element.h"
-class Stack
-{
-	private:
-		Element* _last;
-	public:
-		void push(Element*);
-		Element* pop();
-		Stack();
-};
-
-#endif /* STACK_H_ */
+@@ -17,6 +17,13 @@ class Stack
+  		void push(Element*);
+  		Element* pop();
+  		Stack();
+ +
+ +		 Element* getFirst()
+ +			 {		return _first;	}
+ +
+ +			 Element* getLast()
+ +			 {		return _last;	}
+ +
+  };
+  
+  #endif /* STACK_H_ */
